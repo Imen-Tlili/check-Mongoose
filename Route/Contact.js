b0958cf62fc32f6d58745c24156e85b85c4a56f2
Imen <imentlili2@gmail.com>
2022-03-19 19:43:03 +0100

    first commit

---
+++ Route/Contact.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const { AddContact, GetContact, DeleteContact, UpdateContact } = require('../Controllers/Contact')
+const contact = require('../Model/Contact')
+
+
+
+const ContactRouter=express.Router()
+
+ContactRouter.post('/addContact',AddContact)
+ContactRouter.get('/getContact',GetContact)
+ContactRouter.delete('/deleteContact/:id',DeleteContact)
+ContactRouter.put('/updateContact/:id',UpdateContact)
+
+
+module.exports=ContactRouter
